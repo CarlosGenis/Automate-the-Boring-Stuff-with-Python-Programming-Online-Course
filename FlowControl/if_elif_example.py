3046b08d089bdfea874424908fb37b01481165fa
CarlosGenis <carlosgenis06@gmail.com>
2022-06-03 16:05:20 -0400

    Create if_elif_example.py
    Simple program using elif statement

---
+++ FlowControl/if_elif_example.py
@@ -0,0 +1,11 @@
+#this program uses elif 
+name = "Bobby"
+age = 3000
+if (name == "Bobby"):
+    print("Hey Bobby")
+elif age < 10:
+    print("You are not Bobby, kiddo!")
+elif age > 2000:
+    print("You are older than Bobby")
+elif age > 100:
+    print("You are not Bobby, grannie!")
